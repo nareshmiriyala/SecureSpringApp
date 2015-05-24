6556441064fb7033d3bff429363bf28856cb11f7
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-05-24 16:48:40 +1000

    commit changes

--- spring-boot-sample-web-jsp/src/main/resources/static/js/sample.js
+++ spring-boot-sample-web-jsp/src/main/resources/static/js/sample.js
@@ -1,33 +1,12 @@
-/**
- * Created by nareshm on 22/05/2015.
- */
-
 $(document).ready(function(){
     $("button").click(function(){
-        alert( "Handler for .click() called." );
         var username=$("#username").val();
         var password=$("#password").val();
-        setTimeout(function() {
-            $.ajax({
-                type: "POST",
-                url: "j_security_check",
-                data: { j_username: username, j_password: password }
-            }).done(function(data,status){
-                alert("Data: " + data + "\nStatus: " + status);
-            });
-        }, 2000);
+        $.ajax({ type: "POST",
+            url: "j_security_check",
+            data: { j_username: username, j_password: password }, success: function(result){
+            $("#div1").html(result);
+        }});
     });
 });
 
-//$("#login" ).click(function() {
-//    alert( "Handler for .click() called." );
-//    setTimeout(function() {
-//        $.ajax({
-//            type: "POST",
-//            url: "j_security_check",
-//            data: { j_username: "admin", j_password: "6339098" }
-//        }).done(function( msg ) {
-//            alert( msg );
-//        });
-//    }, 2000);
-//});
